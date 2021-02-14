6aa8acba9c059527c8460d4b4dcc80797d1c69be
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-14 11:39:31 +0530

    comment

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # USAGE
 # python detect_age.py --image images/adrian.png --face face_detector --age age_detector
 
-# import the necessary packages
+
 import numpy as np
 import argparse
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="path to input image")
@@ -19,7 +19,7 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-# define the list of age buckets our age detector will predict
+
 AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
                "(38-43)", "(48-53)", "(60-100)"]
 
@@ -47,29 +47,22 @@ print("[INFO] computing face detections...")
 faceNet.setInput(blob)
 detections = faceNet.forward()
 
-# loop over the detections
 for i in range(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with the
-    # prediction
+
     confidence = detections[0, 0, i, 2]
 
-    # filter out weak detections by ensuring the confidence is
-    # greater than the minimum confidence
+
     if confidence > args["confidence"]:
-        # compute the (x, y)-coordinates of the bounding box for the
-        # object
+
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
 
-        # extract the ROI of the face and then construct a blob from
-        # *only* the face ROI
         face = image[startY:endY, startX:endX]
         faceBlob = cv2.dnn.blobFromImage(face, 1.0, (227, 227),
                                          (78.4263377603, 87.7689143744, 114.895847746),
                                          swapRB=False)
 
-        # make predictions on the age and find the age bucket with
-        # the largest corresponding probability
+
         ageNet.setInput(faceBlob)
         preds = ageNet.forward()
         i = preds[0].argmax()
@@ -80,14 +73,13 @@ for i in range(0, detections.shape[2]):
         text = "{}: {:.2f}%".format(age, ageConfidence * 100)
         print("[INFO] {}".format(text))
 
-        # draw the bounding box of the face along with the associated
-        # predicted age
+
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(image, (startX, startY), (endX, endY),
                       (0, 0, 255), 2)
         cv2.putText(image, text, (startX, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-# display the output image
+
 cv2.imshow("Image", image)
 cv2.waitKey(0)
